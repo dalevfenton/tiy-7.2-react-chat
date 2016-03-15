77738d69d7168eafcca7c935f3d7a7eda04bad9f
Dale Fenton <fentondale@gmail.com>
2016-03-15 17:00:21 -0400

    chat form creates new chat messages

--- app/scripts/components/chatarea.jsx
+++ app/scripts/components/chatarea.jsx
@@ -6,18 +6,60 @@ require('backbone-react-component');
 
 var ChatMessage = React.createClass({
   render: function(){
-    console.log(this.props.model);
     return (
       <div className="message-holder">
         <div className="message">
-          <span className="user message-bubble">{this.props.model.get('content')}</span>
-          <span className="message-sender">{this.props.model.get('username')}</span>
-          <span className="message-time">{this.props.model.get('created_at')}</span>
+          <div className="user">
+            <span className="message-part message-bubble">{this.props.model.get('content')}</span>
+            <span className="message-part message-sender">{this.props.model.get('username')}</span>
+            <span className="message-part message-time">{this.props.model.get('created_at')}</span>
+          </div>
         </div>
       </div>
     );
   }
 })
+
+var ChatForm = React.createClass({
+  getInitialState: function(){
+    return {
+      chat: '',
+    }
+  },
+  onInput: function(e){
+    this.setState({
+      chat: e.target.value
+    })
+  },
+  onSubmit: function(e){
+    e.preventDefault();
+    var chat = this.state.chat;
+    //TODO: setup user "login" to tag messages
+    //username should be held in router at login and passed through component chain
+    this.props.collection.create({
+      content: chat,
+      username: '',
+      created_at: Date.now()
+    });
+    this.setState({ chat: '' });
+    console.log(this.props.collection);
+  },
+  render: function(){
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          name="new_message"
+          value={this.state.chat}
+          onChange={this.onInput}
+        />
+        <button type="submit" name="button">
+          <span className="glyphicon glyphicon-comment" aria-hidden="true"></span>
+        </button>
+      </form>
+    );
+  }
+});
 
 var ChatArea = React.createClass({
   mixins: [Backbone.React.Component.mixin],
@@ -33,12 +75,7 @@ var ChatArea = React.createClass({
           {messageList}
         </div>
         <div className="chat-form">
-          <form>
-            <input type="text" name="new_message" />
-            <button type="submit" name="button">
-              <span className="glyphicon glyphicon-comment" aria-hidden="true"></span>
-            </button>
-          </form>
+          <ChatForm collection={this.props.collection} />
         </div>
       </div>
     );
